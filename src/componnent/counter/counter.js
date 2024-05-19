c5637c46d664e92f120a85f6dbf17e0580f08b1c
Alireza Jalili <alirlikh@gmail.com>
2024-05-19 16:21:30 +0330

    feat: add redux redux-toolkit

---
+++ src/componnent/counter/counter.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "../../store/counterSlice"
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/store/counterSlice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { formToJSON } from "axios"
+
+const initialSate = {
+  value: 0
+}
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialSate,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, () => {
+        console.log("pending...")
+      })
+      .addCase(incrementAsync.fulfilled, (state) => {
+        state.value += 1
+      })
+  }
+})
+
+export const incrementAsync = createAsyncThunk("counter/incermentAsync", async (amount) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return amount
+})
+
+export const { increment, decrement } = counterSlice.actions
+
+export default counterSlice.reducer
