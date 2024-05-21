33362105d111a464881e686e4f3ff8ef3837489b
Alireza Jalili <alirlikh@gmail.com>
2024-05-21 22:33:22 +0330

    feat: add yup

--- src/componnent/sign/signIn.jsx
+++ src/componnent/sign/signIn.jsx
@@ -7,26 +7,44 @@ import backArrow from "../ui/backArrow.svg"
 import { Link, useNavigate } from "react-router-dom"
 import { loginUser } from "../../services/user/user.api"
 import { login } from "../../services/auth"
+import * as Yup from "yup"
 
 function SignIn(props) {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState({})
   const navigate = useNavigate()
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required("username is Required!"),
+    password: Yup.string().required("Password is required!")
+  })
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const { profileInfo } = await login({ username, password })
-    if (profileInfo) {
-      setUsername("")
-      setPassword("")
-      // navigate("/dashboard", { replace: true })
-      window.location.href = "/dashboard"
-    } else alert("error")
+    try {
+      const isValid = await validationSchema.validate({ username, password }, { abortEarly: false })
+      setErrors({})
+      if (isValid) {
+        const { profileInfo } = await login({ username, password })
+        if (profileInfo) {
+          setUsername("")
+          setPassword("")
+          // navigate("/dashboard", { replace: true })
+          window.location.href = "/dashboard"
+        } else alert("error")
+      }
+    } catch (error) {
+      let formErrors = {}
+      error.inner.map((err) => {
+        formErrors[err.path] = err.message
+      })
+      setErrors(formErrors)
+    }
   }
 
   return (
     <>
-      {/* <div className="wizard-item" id="loginP"> */}
       <div className="w-section">
         <div className="login-container">
           <h2>LOGIN PAGE</h2>
@@ -39,32 +57,32 @@ function SignIn(props) {
       </div>
       <div className="w-section login-field" style={{ zIndex: 3 }}>
         <form method="post" onSubmit={handleSubmit}>
-          <label htmlFor="username">
-            <input
-              className="input"
-              name="username"
-              placeholder="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <label htmlFor="password">
-            <input
-              className="input"
-              name="password"
-              placeholder="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
+          <label htmlFor="username"></label>
+          <input
+            className="input"
+            name="username"
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {errors.username && <div className="error">{errors.username}</div>}
+          <label htmlFor="password"></label>
+          <input
+            className="input"
+            name="password"
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && <div className="error">{errors.password}</div>}
+
           <Button>Enter</Button>
         </form>
 
         <Link>Sign Up</Link>
       </div>
-      {/* </div> */}
     </>
   )
 }
--- src/componnent/sign/signUp.jsx
+++ src/componnent/sign/signUp.jsx
@@ -1,14 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "../button/Button"
 import Input from "../input/Input"
 import loginSvg from "../ui/loginSvg.svg"
 import nextArrow from "../ui/nextArrow.svg"
 import backArrow from "../ui/backArrow.svg"
+import * as Yup from "yup"
 
 function SignUp(props) {
+  const initialData = {
+    name: "",
+    mobile: "",
+    password: "",
+    confirmPassword: ""
+  }
+  const [formData, setFormData] = useState(initialData)
+  const [errors, setErrors] = useState({})
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("name is Required!"),
+    mobile: Yup.string()
+      .matches(/^\d{10}$/, "Phone Number must be 10 digits!")
+      .required(),
+    password: Yup.string()
+      .required("Password is required!")
+      .min(8, "Password must be at least 8 characters!")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one symbol!")
+      .matches(/[0-9]/, "Password must contain at least one number!")
+      .matches(/[A-Z]/, "Password must contain at least one uppercase letter!")
+      .matches(/[a-z]/, "Password must contain at least one lowercase letter!"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password")], "Passwords must match!")
+      .required("Confirm password is required!")
+  })
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await validationSchema.validate(formData, { abortEarly: false })
+      setErrors({})
+    } catch (error) {
+      let formErrors = {}
+      error.inner.map((err) => {
+        formErrors[err.path] = err.message
+      })
+      setErrors(formErrors)
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
   return (
     <>
-      {/* <div className="wizard-item" id="signupP"> */}
       <div className="w-section">
         <div className="signup-container">
           <h2>SIGNUP PAGE</h2>
@@ -24,23 +72,50 @@ function SignUp(props) {
         </div>
       </div>
       <div className="w-section login-field" style={{ zIndex: 1 }}>
-        <form action="" method="post">
+        <form method="post" onSubmit={handleSubmit}>
           <label htmlFor="name">
-            <input className="input" name="name" placeholder="Name" />
+            <input
+              className="input"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+            />
           </label>
+          {errors.name && <div className="error">{errors.name}</div>}
           <label htmlFor="mobile">
-            <input className="input" name="mobile" placeholder="Mobile" />
+            <input
+              className="input"
+              name="mobile"
+              placeholder="Mobile"
+              value={formData.mobile}
+              onChange={handleChange}
+            />
           </label>
+          {errors.mobile && <div className="error">{errors.mobile}</div>}
           <label htmlFor="password">
-            <input className="input" name="password" placeholder="Password" />
+            <input
+              className="input"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </label>
+          {errors.password && <div className="error">{errors.password}</div>}
           <label htmlFor="password">
-            <input className="input" name="password" placeholder="Confirm Password" />
+            <input
+              className="input"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
           </label>
+          {errors.confirmPassword && <div className="error">{errors.confirmPassword}</div>}
+          <button className="btn signup-btn">Sign UP</button>
         </form>
-        <button className="btn signup-btn">Sign UP</button>
       </div>
-      {/* </div> */}
     </>
   )
 }
